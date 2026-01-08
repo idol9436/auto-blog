fd52ca78f0fffcd94a5990b748e6df6c22df0a38
sangsang <idolphin2020@gmail.com>
2026-01-08 22:05:21 +0900

    security 아주조금

--- src/main/java/com/sangsang/autoblog/infra/config/SecurityConfig.java
+++ src/main/java/com/sangsang/autoblog/infra/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.sangsang.autoblog.infra.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -16,7 +17,13 @@ public class SecurityConfig {
         http
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/h2-console/**").permitAll()
-                .anyRequest().permitAll()
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll() // static 자원에 대한 접근 모두 허용
+                .requestMatchers("/src/**").permitAll()
+                .requestMatchers("/", "/home", "/healthCheck", "/auth/**").permitAll()
+                .requestMatchers("/content/preview").permitAll()
+                // .requestMatchers("/admin/**").hasRole("ADMIN")
+                .anyRequest().authenticated()
+                // .anyRequest().permitAll()
             )
             .csrf(csrf -> csrf.disable())
             .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin));
