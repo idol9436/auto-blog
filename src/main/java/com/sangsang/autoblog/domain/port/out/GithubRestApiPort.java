580859acbe6e77124b523f24aecd5f3ca0f54d7d
idolphin <idolphin2020@gmail.com>
2026-01-28 22:20:29 +0900

    Mono<String> -> Mono<ResponseEntity<String>>

--- src/main/java/com/sangsang/autoblog/domain/port/out/GithubRestApiPort.java
+++ src/main/java/com/sangsang/autoblog/domain/port/out/GithubRestApiPort.java
@@ -1,11 +1,13 @@
 package com.sangsang.autoblog.domain.port.out;
 
+import org.springframework.http.ResponseEntity;
+
 import com.sangsang.autoblog.domain.model.GithubInfo;
 
 import reactor.core.publisher.Mono;
 
 public interface GithubRestApiPort {
-    public Mono<String> getExistInfo(GithubInfo info);
-    public Mono<String> createToGithub(GithubInfo info);
-    public Mono<String> updateToGithub(GithubInfo info);
+    public Mono<ResponseEntity<String>>  getExistInfo(GithubInfo info);
+    public Mono<ResponseEntity<String>>  createToGithub(GithubInfo info);
+    public Mono<ResponseEntity<String>>  updateToGithub(GithubInfo info);
 }
